def12a0355671d092421c4119e97c236a830a8ac
Eduardo Toriani <edutoriani13@gmail.com>
2025-05-06 23:22:24 -0300

    docs: change example to use navito link variants

--- example/public/global.js
+++ example/public/global.js
@@ -2,9 +2,11 @@ const navito = new Navito('/');
 
 const nav = document.querySelector('nav');
 
-const link_to_home = Navito.Link('/', 'Home');
-const link_to_product = Navito.Link('/products/1', 'Product');
-nav.append(link_to_home, link_to_product);
+const link_to_home = Navito.Link('/', 'Home').variantUnderline();
+const link_to_contact = Navito.Link('/contact', 'Contact').variantGhost();
+const link_to_product = Navito.Link('/products/1', 'Product').variantOutline();
+const link_to_products = Navito.Link('/products', 'Product').noVariant();
+nav.append(link_to_home, link_to_contact, link_to_product, link_to_products);
 
 navito.before((ctx) => console.log('Global before hook has called with this context: ', ctx));
 
